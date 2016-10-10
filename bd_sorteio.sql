ca745dfc7797b362062ae2d74cc79afe338c99c1
marciioluucas <marciioluu@gmail.com>
2016-10-09 23:32:26 -0300

    exportada a base de dados. rs

--- bd_sorteio.sql
+++ bd_sorteio.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 08-Out-2016 às 16:54
--- Versão do servidor: 10.1.13-MariaDB
--- PHP Version: 7.0.5
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -17,8 +8,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bd_sorteio`
+-- Database: `bd_sorder`
 --
+CREATE DATABASE IF NOT EXISTS `bd_sorder` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bd_sorder`;
 
 -- --------------------------------------------------------
 
@@ -60,14 +53,24 @@ CREATE TABLE `produto_has_pedido` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `produto_has_propriedades_produto`
+--
+
+CREATE TABLE `produto_has_propriedades_produto` (
+  `produto_id` int(11) NOT NULL,
+  `propriedades_produto_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `propriedades_produto`
 --
 
 CREATE TABLE `propriedades_produto` (
   `id` int(11) NOT NULL,
   `tamanho` varchar(45) NOT NULL,
-  `preco` double(10,2) NOT NULL,
-  `produto_id` int(11) NOT NULL
+  `preco` double(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -88,30 +91,31 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'terter', 'asodashd@gmail.com', 'dsadasd'),
-(2, 'Marcio Lucas', 'marciioluucas@gmail.com', 'jabuticaba123'),
-(3, 'dasdas', 'asdasd@fiodfs.com', 'asdasdasd'),
-(4, 'Joao', 'dasilva@gmail.com', 'jose'),
-(5, 'Pamonh', 'dasoid@fgo.com', 'asdoi'),
-(6, 'dsad', '123123@dasda.com', 'asdasd'),
-(7, 'Teste2', 'dasod@gmail.com', 'dsaodjas'),
-(8, 'undefined', 'undefined', 'undefined'),
-(9, 'undefined', 'undefined', 'undefined'),
-(10, 'undefined', 'undefined', 'undefined'),
-(11, 'AÃ§aÃ­ universitario', 'doaisjds@ioa.com', 'dsaoijdaso'),
-(12, 'undefined', 'undefined', 'undefined'),
-(13, 'undefined', 'undefined', 'undefined'),
-(14, 'undefined', 'undefined', 'undefined'),
-(15, 'undefined', 'undefined', 'undefined'),
-(16, 'undefined', 'undefined', 'undefined'),
-(17, 'undefined', 'undefined', 'undefined'),
-(18, 'undefined', 'undefined', 'undefined'),
-(19, 'undefined', 'undefined', 'undefined'),
-(20, 'undefined', 'undefined', 'undefined'),
-(21, 'Marcio', 'dasds@fdsa.com', 'dasoijdsa'),
-(22, 'fsfsdfs', 'sdfsdf@gmail.com', 'fsdfsdf'),
-(23, 'aaaaa', 'aaaaa', 'aaaa'),
-(24, 'Marcio', 'qweqwe@dad.com', 'qweqwe');
+  (1, 'terter', 'asodashd@gmail.com', 'dsadasd'),
+  (2, 'Marcio Lucas', 'marciioluucas@gmail.com', 'jabuticaba123'),
+  (3, 'dasdas', 'asdasd@fiodfs.com', 'asdasdasd'),
+  (4, 'Joao', 'dasilva@gmail.com', 'jose'),
+  (5, 'Pamonh', 'dasoid@fgo.com', 'asdoi'),
+  (6, 'dsad', '123123@dasda.com', 'asdasd'),
+  (7, 'Teste2', 'dasod@gmail.com', 'dsaodjas'),
+  (8, 'undefined', 'undefined', 'undefined'),
+  (9, 'undefined', 'undefined', 'undefined'),
+  (10, 'undefined', 'undefined', 'undefined'),
+  (11, 'AÃ§aÃ­ universitario', 'doaisjds@ioa.com', 'dsaoijdaso'),
+  (12, 'undefined', 'undefined', 'undefined'),
+  (13, 'undefined', 'undefined', 'undefined'),
+  (14, 'undefined', 'undefined', 'undefined'),
+  (15, 'undefined', 'undefined', 'undefined'),
+  (16, 'undefined', 'undefined', 'undefined'),
+  (17, 'undefined', 'undefined', 'undefined'),
+  (18, 'undefined', 'undefined', 'undefined'),
+  (19, 'undefined', 'undefined', 'undefined'),
+  (20, 'undefined', 'undefined', 'undefined'),
+  (21, 'Marcio', 'dasds@fdsa.com', 'dasoijdsa'),
+  (22, 'fsfsdfs', 'sdfsdf@gmail.com', 'fsdfsdf'),
+  (23, 'aaaaa', 'aaaaa', 'aaaa'),
+  (24, 'Marcio', 'qweqwe@dad.com', 'qweqwe'),
+  (25, 'Marcio', '12312@fsdfok.com', 'dasdas');
 
 --
 -- Indexes for dumped tables
@@ -138,11 +142,18 @@ ALTER TABLE `produto_has_pedido`
   ADD KEY `fk_produto_has_pedido_produto_idx` (`produto_id`);
 
 --
+-- Indexes for table `produto_has_propriedades_produto`
+--
+ALTER TABLE `produto_has_propriedades_produto`
+  ADD PRIMARY KEY (`produto_id`,`propriedades_produto_id`),
+  ADD KEY `fk_produto_has_propriedades_produto_propriedades_produto1_idx` (`propriedades_produto_id`),
+  ADD KEY `fk_produto_has_propriedades_produto_produto1_idx` (`produto_id`);
+
+--
 -- Indexes for table `propriedades_produto`
 --
 ALTER TABLE `propriedades_produto`
-  ADD PRIMARY KEY (`id`,`produto_id`),
-  ADD KEY `fk_tamanho_produto_produto1_idx` (`produto_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `usuario`
@@ -158,7 +169,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- Constraints for dumped tables
 --
@@ -171,10 +182,11 @@ ALTER TABLE `produto_has_pedido`
   ADD CONSTRAINT `fk_produto_has_pedido_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `propriedades_produto`
+-- Limitadores para a tabela `produto_has_propriedades_produto`
 --
-ALTER TABLE `propriedades_produto`
-  ADD CONSTRAINT `fk_tamanho_produto_produto1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `produto_has_propriedades_produto`
+  ADD CONSTRAINT `fk_produto_has_propriedades_produto_produto1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_produto_has_propriedades_produto_propriedades_produto1` FOREIGN KEY (`propriedades_produto_id`) REFERENCES `propriedades_produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
